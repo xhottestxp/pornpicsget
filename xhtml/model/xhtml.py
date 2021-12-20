50283cea7a0ca0d37dabe5fb669df154069131c6
xhottestxp <xhottestxplus@gmail.com>
2021-12-19 21:32:50 -0300

    xhtml structure and model created

---
+++ xhtml/model/xhtml.py
@@ -0,0 +1,31 @@
+class XHtml:
+    """This class is a model to download a
+    page from Pornpics in HTML.
+
+    Author:
+        xhottestxp
+    """
+
+    def __init__(self):
+        """New Pornpics page html download.
+        """
+        self._urlink = ''
+        self._path = ''
+
+    # getter and setter
+
+    @property
+    def urlink(self):
+        return self._urlink
+
+    @urlink.setter
+    def urlink(self, urlink: urlink):
+        self._urlink = urlink
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, path: str):
+        self._path = path
