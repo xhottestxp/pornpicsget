e58cd9334afae660843301125d6524d07f992230
xhottestxp <xhottestxplus@gmail.com>
2021-12-20 22:01:19 -0300

    service download pornpics

--- pornpics/service/servicepornpics.py
+++ pornpics/service/servicepornpics.py
@@ -1,4 +1,8 @@
+from os import mkdir
+# local project modules
+from core.singleton.smsg import SMsg as Msg
 from pornpics.model.pornpics import Pornpics
+from tools.tools import isdirectory, getnameurl, checkurl
 
 
 class ServicePornpics:
@@ -24,4 +28,22 @@ class ServicePornpics:
         Returns:
             bool: True if success.
         """
-        pass
+        if not isdirectory(pathdir=pornpics.path):
+            # invalid directory - return False
+            Msg.msg().warning = True
+            Msg.msg().title = 'Warning - Path'
+            Msg.msg().message = 'Warning! This path is invalid'
+            return False
+        for data in pornpics.photos[1:]:
+            # checks if there's a invalid imagem url link
+            if not checkurl(urlink=data):
+                Msg.msg().warning = True
+                Msg.msg().title = 'Warning - URL problem'
+                Msg.msg().message = 'Warning! This URL is invalid'
+                return False
+        if pornpics.photos[0].__len__() > 0:
+            # creating a new folder with name got from pornpics.com
+            pornpics.path += f'/{pornpics.photos[0]}'
+            mkdir(pornpics.path)
+        # taking the first element - name folder new
+        pornpics.photos = pornpics.photos[1:]
